be1f1e76a739af6b527517b116fc13dfff8bcdee
karnansp36 <reviewmaster36@gmail.com>
2026-04-29 12:54:46 +0530

    n

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media') # BASE_DIR / 'media' 
